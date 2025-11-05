2caddd6eb56edcdfbca6ad425fa12f8171887f64
123123 <1231231gmail.com>
2025-11-05 09:50:36 +0200

    changed bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,19 @@
-# 1
-# python.exe -m pip install --upgrade pip
-# or
-# pip install --upgrade pip
-import datetime
-# 2
-# python.exe -m pip install -r requirements.txt
-# or
-# pip install -r requirements.txt
-
-
+import time
 
 import requests
 import random
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-bot_token = os.getenv("TOKEN")
+bot_key = '8549148937:AAH19CQ4Hyp9eK1q8jC9dKTENpvSdePiaTQ'
 
-url = f"https://api.telegram.org/bot{bot_token}/"  # don't forget to change the token!
-
-bot_key = '8549148937:AAH19CQ4Hyp9eK1q8jC9dKTENpvSdePiaTQ'  # don't forget to change the token!
+url = f"https://api.telegram.org/bot{bot_key}/"  # don't forget to change the token!
 
 
 def last_update(request):
     response = requests.get(request + 'getUpdates')
-    print(response)
+    # TODO: Uncomment just for local testing
+    # print(response)
     response = response.json()
-    print(response)
+    # print(response)
     results = response['result']
     total_updates = len(results) - 1
     return results[total_updates]
@@ -50,38 +36,33 @@ def send_message(chat, text):
 
 
 def main():
-    update_id = last_update(url)['update_id']
-    while True:
-        # pythonanywhere
-        update = last_update(url)
-        if update_id == update['update_id']:
-            if get_message_text(update).lower() == 'hi' or get_message_text(
-                    update).lower() == 'hello' or get_message_text(update).lower() == 'hey':
-                send_message(get_chat_id(update), 'Greetings! Type "Dice" to roll the dice!')
-            elif get_message_text(update).lower() == 'qa24':
-                send_message(get_chat_id(update), 'Python')
-            elif get_message_text(update).lower() == 'python':
-                send_message(get_chat_id(update), 'version 3.10')
-            elif get_message_text(update).lower() == 'dice':
-                _1 = random.randint(1, 6)
-                _2 = random.randint(1, 6)
-                send_message(get_chat_id(update),
-                             'You have ' + str(_1) + ' and ' + str(_2) + '!\nYour result is ' + str(_1 + _2) + '!')
-            elif get_message_text(update).lower() == 'who':
-                send_message(get_chat_id(update),
-                             "I dont know who are u??"
-                             )
-            elif get_message_text(update).lower() == 'show':
-                send_message(get_chat_id(update),
-                             f"What do u want me to show?"
-                             )
-            elif get_message_text(update).lower() == 'time':
-                send_message(get_chat_id(update),
-                             f"{datetime.time}"
-                             )
-            else:
-                send_message(get_chat_id(update), 'Sorry, I don\'t understand you :(')
-            update_id += 1
+    try:
+        update_id = last_update(url)['update_id']
+        while True:
+            # pythonanywhere
+            time.sleep(3)
+            update = last_update(url)
+            if update_id == update['update_id']:
+                if get_message_text(update).lower() == 'hi' or get_message_text(
+                        update).lower() == 'hello' or get_message_text(update).lower() == 'hey':
+                    send_message(get_chat_id(update), 'Greetings! Type "Dice" to roll the dice!')
+                elif get_message_text(update).lower() == 'csc31':
+                    send_message(get_chat_id(update), 'Python')
+                elif get_message_text(update).lower() == 'gin':
+                    send_message(get_chat_id(update), 'Finish')
+                    break
+                elif get_message_text(update).lower() == 'python':
+                    send_message(get_chat_id(update), 'version 3.10')
+                elif get_message_text(update).lower() == 'dice':
+                    _1 = random.randint(1, 6)
+                    _2 = random.randint(1, 6)
+                    send_message(get_chat_id(update),
+                                 'You have ' + str(_1) + ' and ' + str(_2) + '!\nYour result is ' + str(_1 + _2) + '!')
+                else:
+                    send_message(get_chat_id(update), 'Sorry, I don\'t understand you :(')
+                update_id += 1
+    except KeyboardInterrupt:
+        print('\nБот зупинено')
 
 
 # print(__name__)
